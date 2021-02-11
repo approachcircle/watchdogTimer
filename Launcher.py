90af5d4512cdc93f4810be60108649e5f4ef2405
approachcircle <osc-mcd@protonmail.com>
2021-02-11 01:12:28 +0000

    fixed text formatting and typos

--- Launcher.py
+++ Launcher.py
@@ -1,6 +1,7 @@
-import os, time
+import os
+
 os.system("if exist a.dat (echo) else (echo Note that the session lock file needs to be deleted after the program is closed, in a real watchdog timer, the process never properly ends.)")
 os.system("if exist a.dat (echo) else (pause)")
 os.system("echo > a.dat")
-os.system("queryExist.bat") #since i wanna be fancy and use multi language files, im gonna just execute this in a seperate batch file
-#instead of just doing it here inline
+os.system("queryExist.bat")  # since i wanna be fancy and use multi language files, im gonna just execute this in a
+# separate batch file instead of just doing it here inline
--- watchdogTimer.py
+++ watchdogTimer.py
@@ -1,15 +1,22 @@
-import traceback, time, os, sys, winsound
+import os
+import sys
+import time
+import traceback
+import winsound
+
 os.system("echo > SESSION.LOCK")
 z = 0
 x = 1
 try:
     t = 5
-    wdid = 0
+
+
     def countdown(t):
         while t > z:
             print(t)
             t -= x
             time.sleep(1)
+
 
     while True:
         f = open("!watchdogs/1.$WDSTATE$", "r")
@@ -34,13 +41,12 @@ try:
                 raise ZeroDivisionError
         else:
             raise ZeroDivisionError
-    os.system("del SESSION.LOCK")
 
 except FileNotFoundError:
     print("TRACEBACK BUGCHECK")
-    print("A fatal error occured and the program needs immediate attention.")
+    print("A fatal error occurred and the program needs immediate attention.")
     print("The watchdog state file which is required is missing. (at the path of C:/Windows/!watchdogs/)")
-    winsound.Beep(500,950)
+    winsound.Beep(500, 950)
     input("Press enter to see the stacktrace information:")
     print()
     traceback.print_exc()
@@ -50,15 +56,15 @@ except FileNotFoundError:
 
 except ZeroDivisionError:
     print("TRACEBACK BUGCHECK")
-    print("A fatal error occured and the program needs to restart in order to continue operation.")
+    print("A fatal error occurred and the program needs to restart in order to continue operation.")
     print()
     print("WATCHDOG_VIOLATION")
     print()
-    winsound.Beep(500,950)
+    winsound.Beep(500, 950)
     time.sleep(0.25)
-    winsound.Beep(500,950)
+    winsound.Beep(500, 950)
     time.sleep(0.25)
-    winsound.Beep(500,950)
+    winsound.Beep(500, 950)
     print("DUMPING CRASH INFO. DO NOT TURN OFF YOUR COMPUTER...")
     f = open("!watchdogs/log.bin", "w")
     f.write("WATCHDOG_VIOLATION AT " + time.ctime())
@@ -76,11 +82,11 @@ except ZeroDivisionError:
 
 except Exception:
     print("TRACEBACK BUGCHECK")
-    print("A fatal error occured and the program needs immediate attention.")
+    print("A fatal error occurred and the program needs immediate attention.")
     print("the program cannot identify the problem.")
-    winsound.Beep(500,950)
+    winsound.Beep(500, 950)
     time.sleep(0.25)
-    winsound.Beep(500,950)
+    winsound.Beep(500, 950)
     input("Press enter to see the stacktrace information:")
     print()
     print("Fell back to General Exception SuperClass")
